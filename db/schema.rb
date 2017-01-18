416b08fc8e62cd823d4651b7cced47b44fe6dc20
FAVIO VELEZ <favio.velez@hotmail.com>
2017-01-17 22:32:20 -0600

    devise without password reset working ok

--- db/schema.rb
+++ db/schema.rb
@@ -29,22 +29,19 @@ ActiveRecord::Schema.define(version: 20170115023126) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 4000, default: "", null: false
-    t.string   "encrypted_password",     limit: 4000, default: "", null: false
-    t.string   "reset_password_token",   limit: 4000
-    t.datetime "reset_password_sent_at"
+    t.string   "email",               limit: 4000, default: "", null: false
+    t.string   "encrypted_password",  limit: 4000, default: "", null: false
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,    default: 0,  null: false
+    t.integer  "sign_in_count",       limit: 4,    default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 4000
-    t.string   "last_sign_in_ip",        limit: 4000
-    t.datetime "created_at",                                       null: false
-    t.datetime "updated_at",                                       null: false
+    t.string   "current_sign_in_ip",  limit: 4000
+    t.string   "last_sign_in_ip",     limit: 4000
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   add_foreign_key "galleries", "products"
 end
